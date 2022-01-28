05d2d68aa8a8a66bb57386444bcea9755aabfff9
Travis <tssund93@gmail.com>
2022-01-28 00:03:47 -0600

    refactor calc and add suffix ops

--- Calculator.fs
+++ Calculator.fs
@@ -1,24 +1,27 @@
 [<RequireQualifiedAccess>]
 module Calculator
 
-type BinaryOp =
-    | Plus
-    | Minus
-    | Times
-    | Divide
-    | Exponent
-
-type PrefixOp =
-    | Sqrt
-    | Log
-
-type Expr =
-    | Binary of Expr * BinaryOp * Expr
-    | Prefix of PrefixOp * Expr
-    | Val of float
-
-module private Internal =
+module Internal =
     open FParsec
+
+    type BinaryOp =
+        | Plus
+        | Minus
+        | Times
+        | Divide
+        | Exponent
+
+    type PrefixOp =
+        | Sqrt
+        | Log
+
+    type SuffixOp = | Percent
+
+    type Expr =
+        | Binary of Expr * BinaryOp * Expr
+        | Prefix of PrefixOp * Expr
+        | Suffix of Expr * SuffixOp
+        | Val of float
 
     type Parser<'a> = Parser<'a, unit>
 
@@ -45,46 +48,57 @@ module private Internal =
                      stringReturn "log" Log ]
         .>> spaces
 
+    let suffixOp: Parser<SuffixOp> =
+        spaces >>. choice [ charReturn '%' Percent ]
+        .>> spaces
+
     let value: Parser<Expr> = spaces >>. pfloat .>> spaces |>> Val
 
-    let single, singleRef = createParserForwardedToRef ()
+    let prefixExpr expr : Parser<Expr> =
+        spaces
+        >>. pipe2 prefixOp (expr None) (fun op v -> Prefix(op, v))
+        .>> spaces
+
+    let suffixExpr expr lhs : Parser<Expr> =
+        attempt suffixOp
+        >>= fun op -> expr (Some(Suffix(lhs, op)))
+
+    let parenExpr expr lhs : Parser<Expr> =
+        between (pchar '(') (pchar ')') (spaces >>. expr lhs .>> spaces)
+
+    let single expr =
+        value <|> prefixExpr expr <|> parenExpr expr None
+
+    let binaryExpr expr lhs =
+        parse {
+            let! op = binaryOp
+            let p = prec op
+            let! rhs = single expr
+            let! nextOp = lookAhead (opt binaryOp)
+
+            let nextPrecIsHigher =
+                nextOp
+                |> Option.map (fun nop -> prec nop > p)
+                |> Option.defaultValue false
+
+            if nextPrecIsHigher then
+                return! expr (Some rhs) |>> fun e -> Binary(lhs, op, e)
+            else
+                return! expr (Some(Binary(lhs, op, rhs)))
+        }
 
     let rec expr: option<Expr> -> Parser<Expr> =
         function
         | None ->
             parse {
-                let! lhs = single
+                let! lhs = single expr
                 return! expr (Some lhs) <|> preturn lhs
             }
         | Some lhs ->
-            parse {
-                let! op = binaryOp
-                let p = prec op
-                let! rhs = single
-                let! nextOp = lookAhead (opt binaryOp)
-
-                let nextPrecIsHigher =
-                    nextOp
-                    |> Option.map (fun nop -> prec nop > p)
-                    |> Option.defaultValue false
-
-                if nextPrecIsHigher then
-                    return! expr (Some rhs) |>> fun e -> Binary(lhs, op, e)
-                else
-                    return! expr (Some(Binary(lhs, op, rhs)))
-            }
-            <|> single
-            <|> preturn lhs
-
-    let parenExpr lhs : Parser<Expr> =
-        between (pchar '(') (pchar ')') (spaces >>. expr lhs .>> spaces)
-
-    let prefixExpr: Parser<Expr> =
-        spaces
-        >>. pipe2 prefixOp (expr None) (fun op v -> Prefix(op, v))
-        .>> spaces
-
-    singleRef.Value <- value <|> prefixExpr <|> parenExpr None
+            choice [ suffixExpr expr lhs
+                     binaryExpr expr lhs
+                     single expr
+                     preturn lhs ]
 
     let rec reduceExpr: Expr -> float =
         function
@@ -96,6 +110,7 @@ module private Internal =
         | Binary (e1, Exponent, e2) -> reduceExpr e1 ** reduceExpr e2
         | Prefix (Sqrt, e) -> sqrt (reduceExpr e)
         | Prefix (Log, e) -> log (reduceExpr e)
+        | Suffix (e, Percent) -> (reduceExpr e) * 0.01
 
     let rec exprStr: Expr -> string =
         function
@@ -107,6 +122,7 @@ module private Internal =
         | Binary (e1, Exponent, e2) -> sprintf "(%s ^ %s)" (exprStr e1) (exprStr e2)
         | Prefix (Sqrt, e) -> sprintf "sqrt %s" (exprStr e)
         | Prefix (Log, e) -> sprintf "log %s" (exprStr e)
+        | Suffix (e, Percent) -> sprintf "%s%%" (exprStr e)
 
     let parseExpr: Parser<Expr> = expr None .>> eof
 
