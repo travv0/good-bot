51cd7d34a86338cd92aaf62763a7086ad902e0c5
Travis <tssund@gmail.com>
2022-01-29 01:26:34 -0600

    fix a confusing parse rule

--- Extensions.fs
+++ Extensions.fs
@@ -16,5 +16,28 @@ type CommandContext with
 type DiscordUser with
     member user.IsCarl = user.Id = 235148962103951360UL
 
+[<RequireQualifiedAccess>]
 module String =
     let join (s: string) (strs: seq<string>) = String.Join(s, strs)
+
+[<RequireQualifiedAccess>]
+module Result =
+    let unwrap =
+        function
+        | Ok v -> v
+        | Error e -> failwithf "%s" e
+
+    let unwrapError =
+        function
+        | Ok v -> failwithf "%A" v
+        | Error e -> e
+
+    let isOk =
+        function
+        | Ok v -> true
+        | Error e -> false
+
+    let isError =
+        function
+        | Ok v -> false
+        | Error e -> true
--- test/Tests.fs
+++ test/Tests.fs
@@ -1,45 +1,40 @@
 module Tests
 
-open System
+open Extensions
 open Xunit
-
-let unwrapOk =
-    function
-    | Ok v -> v
-    | Error e -> failwithf "%s" e
-
-let unwrapError =
-    function
-    | Ok v -> failwithf "%A" v
-    | Error e -> e
 
 [<Fact>]
 let ``calc evals correctly`` () =
-    Assert.InRange(Calculator.eval "9 * 18%" |> unwrapOk, 1.62 - 0.01, 1.62 + 0.01)
-    Assert.InRange(Calculator.eval "18%" |> unwrapOk, 0.18 - 0.001, 0.18 + 0.001)
-    Assert.Equal(12., Calculator.eval "3 * sqrt(2 ^ 4)" |> unwrapOk)
+    Assert.InRange(Calculator.eval "9 * 18%" |> Result.unwrap, 1.62 - 0.01, 1.62 + 0.01)
+    Assert.InRange(Calculator.eval "18%" |> Result.unwrap, 0.18 - 0.001, 0.18 + 0.001)
+    Assert.Equal(12., Calculator.eval "3 * sqrt(2 ^ 4)" |> Result.unwrap)
 
     Assert.InRange(
         Calculator.eval "(1 + 2 * 3 + (1 + 2) * 3 + 1 + (2 * 3))^69"
-        |> unwrapOk,
+        |> Result.unwrap,
         9.103757594e+93 - 1e+90,
         9.103757594e+93 + 1e+90
     )
 
     Assert.InRange(
         Calculator.eval "1 + (sin(23.5! * 2) / 5%) - -3213213"
-        |> unwrapOk,
-        3213213.0656864746 - 1.,
-        3213213.0656864746 + 1.
+        |> Result.unwrap,
+        3213213.0656864746 - 100.,
+        3213213.0656864746 + 100.
     )
 
-    Assert.Equal(6., Calculator.eval "abs - 6" |> unwrapOk)
-    Assert.InRange(Calculator.eval "abs - 6 % + 5!" |> unwrapOk, 120.06 - 0.01, 120.06 + 0.01)
-    Assert.InRange(Calculator.eval "abs 6 % + 5!" |> unwrapOk, 120.06 - 0.01, 120.06 + 0.01)
-    Assert.Equal(Calculator.eval "sin 5%" |> unwrapOk, Calculator.eval "sin (5%)" |> unwrapOk)
-    Assert.NotEqual(Calculator.eval "sin 5%" |> unwrapOk, Calculator.eval "(sin 5)%" |> unwrapOk)
-    Assert.Equal(1., Calculator.eval "- 2 + 3" |> unwrapOk)
-    Assert.Equal(-1., Calculator.eval "- 2 + 1" |> unwrapOk)
+    Assert.True(Calculator.eval "abs - 6" |> Result.isError)
+    Assert.Equal(6., Calculator.eval "abs (- 6)" |> Result.unwrap)
+    Assert.True(Calculator.eval "abs - 6 % + 5!" |> Result.isError)
+    Assert.True(Calculator.eval "abs (- 6) % + 5!" |> Result.isOk)
+    Assert.True(Calculator.eval "abs -6 % + 5!" |> Result.isOk)
+    Assert.InRange(Calculator.eval "abs 6 % + 5!" |> Result.unwrap, 120.06 - 0.01, 120.06 + 0.01)
+    Assert.Equal(Calculator.eval "sin 5%" |> Result.unwrap, Calculator.eval "sin (5%)" |> Result.unwrap)
+    Assert.NotEqual(Calculator.eval "sin 5%" |> Result.unwrap, Calculator.eval "(sin 5)%" |> Result.unwrap)
+    Assert.Equal(1., Calculator.eval "- 2 + 3" |> Result.unwrap)
+    Assert.Equal(-1., Calculator.eval "- 2 + 1" |> Result.unwrap)
+    Assert.True(Calculator.eval "sqrt sqrt 4" |> Result.isError)
+    Assert.True(Calculator.eval "sqrt (sqrt 4)" |> Result.isOk)
 
 [<Fact>]
 let ``calc show helpful parse errors`` () =
@@ -52,7 +47,7 @@ Expecting: floating-point number, '(', '-', 'abs', 'cbrt', 'ceil', 'cos',
 'cosh', 'degrees', 'e', 'fact', 'floor', 'ln', 'log', 'pi', 'radians', 'rand',
 'randf', 'randi', 'round', 'sin', 'sinh', 'sqrt', 'tan' or 'tanh'
 ",
-        Calculator.eval "1 +" |> unwrapError
+        Calculator.eval "1 +" |> Result.unwrapError
     )
 
     Assert.Equal(
@@ -63,7 +58,7 @@ Expecting: floating-point number, '(', '-', 'abs', 'cbrt', 'ceil', 'cos',
 'cosh', 'degrees', 'e', 'fact', 'floor', 'ln', 'log', 'pi', 'radians', 'rand',
 'randf', 'randi', 'round', 'sin', 'sinh', 'sqrt', 'tan' or 'tanh'
 ",
-        Calculator.eval "+" |> unwrapError
+        Calculator.eval "+" |> Result.unwrapError
     )
 
     Assert.Equal(
@@ -74,7 +69,7 @@ Expecting: floating-point number, '(', '-', 'abs', 'cbrt', 'ceil', 'cos',
 'cosh', 'degrees', 'e', 'fact', 'floor', 'ln', 'log', 'pi', 'radians', 'rand',
 'randf', 'randi', 'round', 'sin', 'sinh', 'sqrt', 'tan' or 'tanh'
 ",
-        Calculator.eval " +" |> unwrapError
+        Calculator.eval " +" |> Result.unwrapError
     )
 
     Assert.Equal(
@@ -85,7 +80,7 @@ Expecting: floating-point number, '(', '-', 'abs', 'cbrt', 'ceil', 'cos',
 'cosh', 'degrees', 'e', 'fact', 'floor', 'ln', 'log', 'pi', 'radians', 'rand',
 'randf', 'randi', 'round', 'sin', 'sinh', 'sqrt', 'tan' or 'tanh'
 ",
-        Calculator.eval " + " |> unwrapError
+        Calculator.eval " + " |> Result.unwrapError
     )
 
     Assert.Equal(
@@ -95,7 +90,7 @@ Expecting: floating-point number, '(', '-', 'abs', 'cbrt', 'ceil', 'cos',
 Note: The error occurred at the end of the input stream.
 Expecting: '!', '!!', '%', ')', '*', '+', '-', '/', '^' or 'mod'
 ",
-        Calculator.eval "(1" |> unwrapError
+        Calculator.eval "(1" |> Result.unwrapError
     )
 
     Assert.Equal(
@@ -105,5 +100,5 @@ Expecting: '!', '!!', '%', ')', '*', '+', '-', '/', '^' or 'mod'
 Note: The error occurred at the end of the input stream.
 Expecting: '!', '!!', '%', ')', '*', '+', '-', '/', '^' or 'mod'
 ",
-        Calculator.eval "(1+2" |> unwrapError
+        Calculator.eval "(1+2" |> Result.unwrapError
     )
